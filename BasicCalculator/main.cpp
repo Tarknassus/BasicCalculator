ffe4525dfa151422528a70047900eada51e17cc8
The-Bearded-Vegan <63019033+The-Bearded-Vegan@users.noreply.github.com>
2020-04-02 08:46:16 +0100

    Initial program code

--- BasicCalculator/main.cpp
+++ BasicCalculator/main.cpp
@@ -7,9 +7,21 @@
 //
 
 #include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    string mystr;
+    float price=0;
+    int quantity=0;
+    
+    cout << "Enter price: ";
+    getline (cin, mystr);
+    stringstream(mystr) >> price;
+    cout << "Enter quantity: ";
+    getline (cin, mystr);
+    stringstream(mystr) >> quantity;
+    cout << "Total price: " << price*quantity << endl;
     return 0;
 }
